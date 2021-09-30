95a762b6824d8e8bef107a70907e31f9092761c6
arshadbagwan2 <91364237+arshadbagwan2@users.noreply.github.com>
2021-10-01 00:32:20 +0530

    Update Alexa.py

--- Alexa.py
+++ Alexa.py
@@ -51,6 +51,9 @@ def call_Anuja():
     elif 'are you single' in command:
         cmd = talk('I am in a relationship with WiFi')
         print(cmd)
+    elif 'Hello,Who are you' in command:
+        cmd = talk('I am Arshad')
+        print(cmd)    
     else:
        default = talk('I am sorry, I did not understand')
        print(default)
