c7b221c0125c5a6b312326388758f638ee71244e
Astroakanksha <62433778+Astroakanksha24@users.noreply.github.com>
2021-10-15 12:18:41 +0530

    Update Alexa.py

--- Alexa.py
+++ Alexa.py
@@ -125,6 +125,9 @@ def call_Anuja():
     elif 'Do you believe in god?' in command:
         cmd=talk('Yes, according to definition my god is Anuja, as she created me. I am not sure about your god.')
         print(cmd)
+    elif 'What is the most favourite holiday destination?' in command:
+        cmd=talk('Maldives.')
+        print(cmd)
     else:
        default = talk('I am sorry, I did not understand ')
        print(default)
