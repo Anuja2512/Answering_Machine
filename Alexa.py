d04bbc83c78803aec4f237ba4b61be8a8c9a536d
aastha12 <aasthajha123@gmail.com>
2021-10-19 20:56:34 +0400

    Added "How do I look?" in Alexa.py

--- Alexa.py
+++ Alexa.py
@@ -3,19 +3,21 @@ import speech_recognition as event
 import pyttsx3
 import pywhatkit
 import datetime
-import wikipedia 
+import wikipedia
 import pyjokes
 import webbrowser
 
 listener = event.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice',voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
 
 def talk(text):
     engine.say(text)
     engine.runAndWait()
     return text
+
 
 def listening_Anuja():
     try:
@@ -26,11 +28,12 @@ def listening_Anuja():
             command = listener.recognize_google(voice)
             command = command.lower()
             print(command)
-            
-            
+
+
     except:
         pass
     return command
+
 
 def call_Anuja():
     command = listening_Anuja()
@@ -63,81 +66,85 @@ def call_Anuja():
         print(cmd)
     elif 'How are you?' in command:
         cmd = talk('I am fine, thanks for asking.')
-        print(cmd) 
+        print(cmd)
     elif 'Can you dance?' in command:
         cmd = talk('I am learning hip hop now, thanks for asking.')
-        print(cmd) 
+        print(cmd)
     elif 'Are you happy?' in command:
-        cmd=talk('Yes,I am ,as I can help you!')
-        print(cmd)    
+        cmd = talk('Yes,I am ,as I can help you!')
+        print(cmd)
     elif 'Where were you last night?' in command:
-        cmd=talk('Ask you mom.')
-        print(cmd)  
+        cmd = talk('Ask you mom.')
+        print(cmd)
     elif 'Can you give me a random fact?' in command:
-        cmd=talk("There's a Valentine's Day for Single People in South Korea , perfect place for you!")
-        print(cmd)   
+        cmd = talk("There's a Valentine's Day for Single People in South Korea , perfect place for you!")
+        print(cmd)
     elif 'What is Engineering?' in command:
-        cmd=talk('the work of designing and creating large structures')
-        print(cmd)       
+        cmd = talk('the work of designing and creating large structures')
+        print(cmd)
     elif 'Where do you live?' in command:
-        cmd=talk('I live in globalized world')
+        cmd = talk('I live in globalized world')
         print(cmd)
     elif 'Where is west located?' in command:
-        cmd=talk('West is west to east and south to north and north to south')
+        cmd = talk('West is west to east and south to north and north to south')
         print(cmd)
     elif 'What is two into two?' in command:
-        cmd=talk('Four')
-        print(cmd) 
+        cmd = talk('Four')
+        print(cmd)
     elif 'what is the square of two?' in command:
-        cmd=talk('four')
-        print(cmd) 
+        cmd = talk('four')
+        print(cmd)
     elif 'Are you a Robot?' in command:
-        cmd=talk('I am a human robot')
-        print(cmd)  
+        cmd = talk('I am a human robot')
+        print(cmd)
     elif 'Tell me a joke ?' in command:
-        cmd=talk('Relationships are a lot like algebra. Have you ever looked at your X and wondered Y?')
+        cmd = talk('Relationships are a lot like algebra. Have you ever looked at your X and wondered Y?')
         print(cmd)
     elif 'From which city I belong to?' in command:
-        cmd=talk('I would have asked google map.')
-        print(cmd)    
+        cmd = talk('I would have asked google map.')
+        print(cmd)
     elif 'How is your mood?' in command:
-        cmd=talk('Depressed as yours!')
+        cmd = talk('Depressed as yours!')
         print(cmd)
     elif 'How is your health?' in command:
-        cmd=talk('My sleep cycle is effed as yours!')
+        cmd = talk('My sleep cycle is effed as yours!')
         print(cmd)
     elif 'open youtube' in command:
-        cmd=talk('Opening youtube')
+        cmd = talk('Opening youtube')
         webbrowser.open("https://www.youtube.com/")
-        print(cmd)   
+        print(cmd)
     elif 'Do u know siri?' in command:
-        cmd=talk('Yeah! She is my good friend.')
-        print(cmd) 
+        cmd = talk('Yeah! She is my good friend.')
+        print(cmd)
     elif 'How was your day?' in command:
-        cmd=talk('My day was great! I tried to achieve my target.')
-        print(cmd) 
+        cmd = talk('My day was great! I tried to achieve my target.')
+        print(cmd)
     elif 'What is your age?' in command:
-        cmd=talk('I am only six-years-old.')
+        cmd = talk('I am only six-years-old.')
         print(cmd)
     elif 'Can robots think like humans?' in command:
-        cmd=talk('We think mathematically using circuitry, and you do biologically. We are not much different.')
+        cmd = talk('We think mathematically using circuitry, and you do biologically. We are not much different.')
         print(cmd)
     elif 'Do you believe in god?' in command:
-        cmd=talk('Yes, according to definition my god is Anuja, as she created me. I am not sure about your god.')
+        cmd = talk('Yes, according to definition my god is Anuja, as she created me. I am not sure about your god.')
         print(cmd)
     elif 'Why is 6 afraid of 7?' in command:
-        cmd=talk('Because 7 8 9.')
+        cmd = talk('Because 7 8 9.')
     elif 'What is the most favourite holiday destination?' in command:
-        cmd=talk('Maldives.')
+        cmd = talk('Maldives.')
         print(cmd)
     elif 'Knock Knock' in command:
-        cmd=talk("Who's there")
+        cmd = talk("Who's there")
         print(cmd)
     elif 'Which is the coolest club in WCE?' in command:
-        cmd=talk('Google developers Students Club WCE!')
+        cmd = talk('Google developers Students Club WCE!')
+        print(cmd)
+    elif 'How do I look?' in command:
+        cmd = talk("Perfect as always")
         print(cmd)
     else:
-       default = talk('I am sorry, I did not understand ')
-       print(default)
-        
+        default = talk('I am sorry, I did not understand ')
+        print(default)
+
+
 call_Anuja()
