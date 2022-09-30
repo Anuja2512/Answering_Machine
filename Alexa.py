302527b9f5a9e39b37e0c418581c7a72dd297801
Prathamesh2254 <102605670+Prathamesh2254@users.noreply.github.com>
2022-09-30 20:03:04 +0530

    added about mood

--- Alexa.py
+++ Alexa.py
@@ -159,6 +159,9 @@ def call_Anuja():
     elif 'day' in command:
         cmd=talk(datetime.today().strftime('%A'))
         print(cmd)
+   elif 'what is your mood?' in command:
+        cmd = talk('preety good!!!')
+        print(cmd)
     else:
         default = talk('I am sorry, I did not understand ')
         print(default)
