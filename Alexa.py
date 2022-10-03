9b4048c5a095f2ee03479f56e36b1ea68947ec64
Karthik P M <72903849+Karthik-PM@users.noreply.github.com>
2022-10-03 21:48:34 +0530

    Update Alexa.py

--- Alexa.py
+++ Alexa.py
@@ -15,12 +15,10 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
-
 def talk(text):
     engine.say(text)
     engine.runAndWait()
     return text
-
 
 def listening_Anuja():
     try:
